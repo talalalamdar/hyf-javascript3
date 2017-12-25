ed43d81773da02567e07d113e9724668422e743e
talalalamdar <talalalamdar@gmail.com>
2017-12-25 16:27:54 +0100

    modefying the code

--- week-2/HttpRequests.js
+++ week-2/HttpRequests.js
@@ -4,20 +4,20 @@ let div = document.createElement("div");
 document.body.appendChild(div);
 div.id = "details-div";
 let detailsDiv = document.getElementById("details-div");
+let h2 = document.createElement("h2");
+let h3 = document.createElement("h3");
+let img = document.createElement("img");
+img.className = "profile-pic"
 
 
 
 function checkTheName() {
-
+    
     detailsDiv.innerHTML = "";
     let userName = searchBox.value;
-    let h2 = document.createElement("h2");
-    let h3 = document.createElement("h3");
     detailsDiv.appendChild(h2)
     detailsDiv.appendChild(h3);
-    let img = document.createElement("img");
-    img.className = "profile-pic"
-    
+
     requests();
 
     function requests() {
@@ -27,7 +27,7 @@ function checkTheName() {
                 if (firstRequest.status !== 200) {
                     console.log("not 200");
                     h2.innerHTML = "This username doesn't exist"
-                } 
+                }
                 else {
                     console.log("loaded first request")
                     let data = JSON.parse(firstRequest.responseText);
@@ -47,12 +47,12 @@ function checkTheName() {
                                 let repoName = "";
                                 let data2 = JSON.parse(secondRequest.responseText)
                                 data2.forEach(repo => {
-                                   
+
                                     repoName = repo.name;
                                     let li = document.createElement("li");
-                                    h3.innerHTML = userName + "'s repositories:"
                                     li.innerHTML = repoName;
-                                    
+                                    h3.innerHTML = userName + "'s repositories:"
+
 
                                     let thirdRequest = new XMLHttpRequest();
                                     thirdRequest.onreadystatechange = () => {
@@ -63,13 +63,17 @@ function checkTheName() {
                                             } else {
                                                 console.log("loaded third request");
                                                 let data3 = JSON.parse(thirdRequest.responseText)
-                                                let lastCommit = data3[0]  
+                                                let lastCommit = data3[0]
+                                                let authorImg = document.createElement("img");
+                                                authorImg.src = lastCommit.author.avatar_url;
+                                                authorImg.className = 'author-img'
                                                 h3.appendChild(li);
                                                 let p = document.createElement("p");
                                                 let commitDate = lastCommit.commit.author.date;
                                                 let commitAuthor = lastCommit.commit.author.name
                                                 p.innerHTML = "last commit was at " + commitDate + " by " + commitAuthor
                                                 li.appendChild(p)
+                                                li.appendChild(authorImg);
                                             }
                                         }
                                     }
